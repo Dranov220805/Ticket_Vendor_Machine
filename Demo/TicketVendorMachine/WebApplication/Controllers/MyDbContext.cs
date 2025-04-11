6c67c5599347d4c5c1f0d7a24a8863cdac93c422
Long Duong <duongthanhlong220805@gmail.com>
2025-04-11 08:48:21 +0700

    update  file

---
+++ Demo/TicketVendorMachine/WebApplication/Controllers/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Configuration;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("ConnectionString") { }
+
+        public DbSet<User> tblUsers { get; set; }
+    }
+}
