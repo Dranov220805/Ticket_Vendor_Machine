3ce1c4bfc4b106265c8e4bf6b26f445e0a59fe62
Long Duong <duongthanhlong220805@gmail.com>
2025-04-13 23:19:51 +0700

    update file

---
+++ Demo/TicketVendorMachine/VendorMachineUI/PaymentMethod/QRCodeMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VendorMachineUI.PaymentMethod
+{
+    public partial class QRCodeMethod : Form
+    {
+        public QRCodeMethod()
+        {
+            InitializeComponent();
+        }
+    }
+}
